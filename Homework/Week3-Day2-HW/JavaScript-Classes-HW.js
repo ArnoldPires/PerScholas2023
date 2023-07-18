4c9bca95a9fc8fb817b65f704fe75882ddfe0600
ArnoldPires <arnold.pires92@gmail.com>
2023-07-18 09:25:50 -0400

    Just setting up the homework

---
+++ Homework/Week3-Day2-HW/JavaScript-Classes-HW.js
@@ -0,0 +1,19 @@
+class Cat {}
+//give the class at least 3 properties
+//give the class at least 3 methods
+//create two instances of the class (two cats)
+//log both instances to see their properties
+//invoke each method from both instances
+
+
+class Pirate {
+
+  constructor(){
+
+  }
+
+}
+//create 3 properties that are set by the constructor
+//create 3 methods
+//instantiate 2 arraysof 3 pirates, name the arrays after your favorite pirate ships, jollyRoger and blackPearl
+//loop over each array and print 3 properties of each pirate
